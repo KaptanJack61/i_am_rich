3107b2943d52d4109f7fc481c93f0a5b0962738a
Hamdi Kalaycı <hamdikalayci@gmail.com>
2021-03-01 11:16:28 +0300

    update try

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 
 // Main fonksiyonu uygulamayı başlatır
-void main() {
-  runApp(
-    MaterialApp(
+
+void main() => runApp(IamRich());
+
+class IamRich extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: Text("I Am Rich"),
+          title: Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: Center(
           child: Image(
-            image: NetworkImage("https://www.w3schools.com/w3images/lights.jpg"),
+            image: AssetImage('images/diamond.png'),
           ),
-        )
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
